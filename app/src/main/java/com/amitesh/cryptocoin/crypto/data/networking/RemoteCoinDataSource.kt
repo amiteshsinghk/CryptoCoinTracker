a6f0856ba1a0e871bac2cd3636185276a50bd9b9
Amitesh Singh <amiteshsinghk@gmail.com>
2024-11-28 21:05:45 +0530

    api calling.

--- app/src/main/java/com/amitesh/cryptocoin/crypto/data/networking/RemoteCoinDataSource.kt
+++ app/src/main/java/com/amitesh/cryptocoin/crypto/data/networking/RemoteCoinDataSource.kt
@@ -6,11 +6,17 @@ import com.amitesh.cryptocoin.core.domain.util.NetworkError
 import com.amitesh.cryptocoin.core.domain.util.Result
 import com.amitesh.cryptocoin.core.domain.util.map
 import com.amitesh.cryptocoin.crypto.data.mappers.toCoin
+import com.amitesh.cryptocoin.crypto.data.mappers.toCoinPrice
+import com.amitesh.cryptocoin.crypto.data.networking.dto.CoinsHistoryDto
 import com.amitesh.cryptocoin.crypto.data.networking.dto.CoinsResponseDto
 import com.amitesh.cryptocoin.crypto.domain.Coin
 import com.amitesh.cryptocoin.crypto.domain.CoinDataSource
+import com.amitesh.cryptocoin.crypto.domain.CoinPrice
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 class RemoteCoinDataSource(
     private val httpClient: HttpClient
@@ -25,6 +31,32 @@ class RemoteCoinDataSource(
                 coinDto.toCoin()
             }
 
+        }
+    }
+
+    override suspend fun getCoinHistory(
+        coinId: String,
+        start: ZonedDateTime,
+        end: ZonedDateTime
+    ): Result<List<CoinPrice>, NetworkError> {
+        val startMillis = start.withZoneSameInstant(
+            ZoneId.of("UTC")
+        ).toInstant().toEpochMilli()
+        val endMillis = end.withZoneSameInstant(
+            ZoneId.of("UTC")
+        ).toInstant().toEpochMilli()
+       return safeCall<CoinsHistoryDto> {
+            httpClient.get(
+                urlString = constructUrl("/assets/$coinId/history")
+            ){
+                parameter("interval", "h6")
+                parameter("start", startMillis)
+                parameter("end", endMillis)
+            }
+        }.map {
+            it.data.map { coinPriceDto ->
+                coinPriceDto.toCoinPrice()
+            }
         }
     }
 }

0efd1bb7a6cf0df767599b1b6164954dca19ab5b
Amitesh Singh <amiteshsinghk@gmail.com>
2024-12-02 21:53:36 +0530

    Update.

--- app/src/main/java/com/amitesh/cryptocoin/home/presentation/home_detail/line_chart/ChartStyle.kt
+++ app/src/main/java/com/amitesh/cryptocoin/home/presentation/home_detail/line_chart/ChartStyle.kt
@@ -4,15 +4,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 
+//To configure the chart
 data class ChartStyle(
-    val chartLineColor: Color,
-    val unselectedColor: Color,
-    val selectedColor: Color,
-    val helperLineThickness: Float,
-    val axisLineThicknessPx: Float,
-    val labelFontSize: TextUnit,
-    val minYLabelSpacing: Dp,
-    val verticalPadding: Dp,
-    val horizontalPadding: Dp,
-    val xAxisLabelSpacing: Dp,
+    val chartLineColor: Color, // chart line color
+    val unselectedColor: Color,// unselected item color
+    val selectedColor: Color, // selected item color
+    val helperLineThickness: Float, // vertical and horizontal line thickness
+    val axisLineThicknessPx: Float, // Line for x and y axis
+    val labelFontSize: TextUnit, // Font size of textview
+    val minYLabelSpacing: Dp, // Minimum space between two text view in Y-axis
+    val verticalPadding: Dp, // top padding of the graph
+    val horizontalPadding: Dp, //  bottom padding of the graph
+    val xAxisLabelSpacing: Dp, // space between two textview in X-axis
 )

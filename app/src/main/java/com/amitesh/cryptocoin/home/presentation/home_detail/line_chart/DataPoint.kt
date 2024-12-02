82a8a9de492dd1a19760d0fcb635a4db68c42f66
Amitesh Singh <amiteshsinghk@gmail.com>
2024-12-02 22:07:40 +0530

    Update.

--- app/src/main/java/com/amitesh/cryptocoin/home/presentation/home_detail/line_chart/DataPoint.kt
+++ app/src/main/java/com/amitesh/cryptocoin/home/presentation/home_detail/line_chart/DataPoint.kt
@@ -3,5 +3,5 @@ package com.amitesh.cryptocoin.home.presentation.home_detail.line_chart
 data class DataPoint(
     val x: Float,
     val y: Float,
-    val xLabel: String
+    val xLabel: String // formatted date
 )
